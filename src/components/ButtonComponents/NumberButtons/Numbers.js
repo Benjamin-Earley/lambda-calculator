9fda5c2c086d3445ee086c8cebc1f3ba15288ece
Benjamin Earley <benjaminearley@Benjamins-MacBook-Pro-2.local>
2019-10-25 13:04:03 -0400

    Imports/exports in place.

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,11 +1,8 @@
 import React from "react";
 
-//import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
-*/
-//Import your array data to from the provided data file
+//import any components needed 
+
+import { numbers } from '../../../data' //Import your array data to from the provided data file
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
